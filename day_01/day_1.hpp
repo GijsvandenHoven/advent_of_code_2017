9d400bfbcf174e80c367ee7480c770d61093bbd9
GijsvandenHoven <gijsvandenhoven@proton.me>
2025-12-01 22:38:47 +0100

    Day 1

--- day_01/day_1.hpp
+++ day_01/day_1.hpp
@@ -14,21 +14,61 @@ CLASS_DEF(DAY) {
     DEFAULT_CTOR_DEF(DAY)
 
     void parse(std::ifstream &input) override {
+        std::string line;
+        std::getline(input, line);
+
+        std::ranges::for_each(line, [this](char c)
+        {
+            numbers.emplace_back(c - '0');
+        });
     }
 
+    // not 1994 - high
+    // not 205 - low
     void v1() const override {
-        reportSolution(0);
+        int prev = numbers.back();
+        int count = 0;
+        for (auto v : numbers)
+        {
+            if (prev == v)
+            {
+                count += v;
+            }
+            prev = v;
+        }
+
+        reportSolution(count);
     }
 
     void v2() const override {
-        reportSolution(0);
+        // allowed to assume even number of elements.
+        const int jump_ahead = static_cast<int>(numbers.size() / 2);
+        auto counterpart = [&](int i) -> int
+        {
+            return static_cast<int>((i + jump_ahead) % numbers.size());
+        };
+
+        int count = 0;
+        for (int i = 0; i < numbers.size(); ++i)
+        {
+            int v = numbers.at(i);
+            int other = numbers.at(counterpart(i));
+            std:: cout << v << " , " << other << "\n";
+            if (other == v)
+            {
+                count += v;
+            }
+        }
+
+        reportSolution(count);
     }
 
     void parseBenchReset() override {
+        numbers.clear();
     }
 
     private:
-    // ...
+    std::vector<int> numbers;
 };
 
 } // namespace
