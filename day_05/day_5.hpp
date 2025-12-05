e82dba90fcffa8e9eb10da73c1fb09a813034388
GijsvandenHoven <gijsvandenhoven@proton.me>
2025-12-05 18:04:05 +0100

    d5

--- day_05/day_5.hpp
+++ day_05/day_5.hpp
@@ -9,29 +9,57 @@
 
 NAMESPACE_DEF(DAY) {
 
-    CLASS_DEF(DAY) {
-        public:
-        DEFAULT_CTOR_DEF(DAY)
+CLASS_DEF(DAY) {
+    public:
+    DEFAULT_CTOR_DEF(DAY)
 
-        void parse(std::ifstream &input) override {
-
+    void parse(std::ifstream &input) override {
+        std::string line;
+        while (std::getline(input, line))
+        {
+            offsets.emplace_back(std::stoi(line));
         }
+    }
 
-        void v1() const override {
-            reportSolution(0);
+    static int traverse_and_count_until_exit(std::vector<int>& jumps, bool insane_rules = false)
+    {
+        int at = 0;
+        int step_count = 0;
+        while (at >= 0 && at < jumps.size())
+        {
+            int offset = jumps.at(at);
+
+            if (insane_rules)
+            {
+                jumps.at(at) += (offset >= 3 ? -1 : 1);
+            } else
+            {
+                jumps.at(at) += 1;
+            }
+
+            at += offset;
+            ++step_count;
         }
+        return step_count;
+    }
 
-        void v2() const override {
-            reportSolution(0);
-        }
+    void v1() const override {
+        auto copy = offsets;
+        reportSolution(traverse_and_count_until_exit(copy));
+    }
 
-        void parseBenchReset() override {
-            // none
-        }
+    void v2() const override {
+        auto copy = offsets;
+        reportSolution(traverse_and_count_until_exit(copy, true));
+    }
 
-        private:
-        // ...
-    };
+    void parseBenchReset() override {
+        offsets.clear();
+    }
+
+    private:
+    std::vector<int> offsets;
+};
 
 } // namespace
 
